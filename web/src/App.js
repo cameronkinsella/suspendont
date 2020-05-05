23719b427f2b778c5059760dfbe954c8e111dd2d
Cameron Kinsella <53635597+cameronkinsella@users.noreply.github.com>
2020-05-05 00:00:34 -0230

    Basic column layout complete

--- web/src/App.js
+++ web/src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import './App.css';
-import {icon} from '@fortawesome/fontawesome-svg-core';
-import {faTwitter} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './App.scss';
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
 
 function App() {
   return (
@@ -11,19 +10,19 @@ function App() {
       <div className="sidebar">
         <FontAwesomeIcon icon={faTwitter} className="Twitter"/>
       </div>
+      <div className="columns">
+        <div className="Suspended">
+          <p>
+            Suspended
+          </p>
+        </div>
 
-      <div className="Suspended">
-        <span className="headerSuspended">
-        Suspended
-      </span>
-      </div>
 
-
-      <div className="Deleted">
-        <span className="headerDeleted">
-        Deleted
-      </span>
-
+        <div className="Deleted">
+          <p>
+            Deleted
+          </p>
+        </div>
       </div>
     </div>
   );
