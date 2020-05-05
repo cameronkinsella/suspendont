7cdc5eed516afe729338fc8c30741ab92caafb27
Ethan Tracey <ethanftracey@gmail.com>
2020-05-04 22:07:58 -0230

    Started design.

--- web/src/App.js
+++ web/src/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import './App.css';
+import {icon} from '@fortawesome/fontawesome-svg-core';
+import {faTwitter} from "@fortawesome/free-brands-svg-icons";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+
+      <div className="sidebar">
+        <FontAwesomeIcon icon={faTwitter} className="Twitter"/>
+      </div>
+
+      <div className="Suspended">
+        <span className="headerSuspended">
+        Suspended
+      </span>
+      </div>
+
+
+      <div className="Deleted">
+        <span className="headerDeleted">
+        Deleted
+      </span>
+
+      </div>
     </div>
   );
 }
